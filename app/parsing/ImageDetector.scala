5e700401514d856107dd070c3b15c272a1a2609c
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-04-15 09:09:36 +1200

    Allow basic detected images to be decorated with scheme specific fields.

--- app/parsing/ImageDetector.scala
+++ app/parsing/ImageDetector.scala
@@ -9,31 +9,47 @@ import scala.util.Try
 
 class ImageDetector {
 
-  private val imageProperties = Set("og:image", "twitter:image")
-
   private val validImageUrlSchemes = Set("http", "https")
 
   def detectImagesIn(html: String): Seq[DetectedImage] = {
-    val doc = Jsoup.parse(html)
+    val metaTags = metaTagsAsMap(html)
 
-    val allMetaTags = doc.select("meta").asScala.toSeq
-
-    val imageMetaTags = allMetaTags.filter { e =>
-      val identifier = Seq(Option(e.attr("property")), Option(e.attr("name"))).flatten
-      identifier.exists(imageProperties.contains)
+    val ogImage = for {
+      ogImage <- detectedImageFrom(metaTags, "og:image")
+    } yield {
+      ogImage.copy(contentType = metaTags.get("og:image:type"))
     }
-    imageMetaTags.flatMap { tag =>
-      val proposedUrl = tag.attr("content")
-      val parsedUri = Try(java.net.URI.create(proposedUrl)).toOption
-      parsedUri.flatMap { uri =>
-        onlyFullQualified(uri).map { url =>
-          DetectedImage(url = url.toURL.toExternalForm, contentType = None)
-        }
-      }
+
+    val twitterImage = detectedImageFrom(metaTags, "twitter:image")
+    Seq(ogImage, twitterImage).flatten
+  }
+
+  private def detectedImageFrom(metaTags: Map[String, String], imageIdentifier: String): Option[DetectedImage] = {
+    for {
+      imageContent <- metaTags.get(imageIdentifier)
+      imageUri <- Try(java.net.URI.create(imageContent)).toOption
+      fullyQualifiedImageUri <- onlyFullQualified(imageUri)
+    } yield {
+      DetectedImage(url = fullyQualifiedImageUri.toURL.toExternalForm)
     }
   }
 
-  private def onlyFullQualified(uri: URI) = {
+  private def metaTagsAsMap(html: String): Map[String, String] = {
+    val doc = Jsoup.parse(html)
+    val allMetaTags = doc.select("meta").asScala.toSeq
+
+    allMetaTags.flatMap { e =>
+      for {
+        // We are only interested in key value pair type tags
+        identifier <- Seq(e.attr("property"), e.attr("name")).find(_.nonEmpty)
+        content <- Option(e.attr("content"))
+      } yield {
+        (identifier, content)
+      }
+    }.toMap
+  }
+
+  private def onlyFullQualified(uri: URI): Option[URI] = {
     if (validImageUrlSchemes.contains(uri.getScheme)) {
       Some(uri)
     } else {
