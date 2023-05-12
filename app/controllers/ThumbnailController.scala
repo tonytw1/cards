2b52ef6ee272fda385f38cb23532542f8e9ab6b2
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-05-12 09:58:41 +0100

    Handle 404 for thumbnails with missing pinned image.

--- app/controllers/ThumbnailController.scala
+++ app/controllers/ThumbnailController.scala
@@ -23,23 +23,27 @@ class ThumbnailController @Inject()(val controllerComponents: ControllerComponen
   private val imageProxyUrl = configuration.get[String]("imageproxy.url")
 
   def thumbnail(url: String): Action[AnyContent] = Action.async {
-    logger.info("Fetching pinned url: " + url)
     val validURL = Try(new URL(url)).toOption
-
     validURL.map { url =>
       val contentFile = new File(filePathForContent(url))
       if (contentFile.exists()) {
-        val eventualResizedContent = resize(url)
-
         val mayContentType = contentTypeOfPinned(url)
-        eventualResizedContent.map { resizedContent =>
-          val contentLength = resizedContent.length
-          logger.info(s"Returning thumbnail of local file ${filePathForContent(url)} with length $contentLength")
-          Ok.sendEntity(HttpEntity.Strict(ByteString.apply(resizedContent), mayContentType)).withHeaders("Cache-Control" -> "max-age=3600")
+
+        val eventualMaybeResizedContent = resize(url)
+
+        eventualMaybeResizedContent.map { maybeResizedContent =>
+          maybeResizedContent.map { resizedContent =>
+            val contentLength = resizedContent.length
+            logger.info(s"Returning thumbnail of local file ${filePathForContent(url)} with length $contentLength")
+            Ok.sendEntity(HttpEntity.Strict(ByteString.apply(resizedContent), mayContentType)).withHeaders("Cache-Control" -> "max-age=3600")
+
+          }.getOrElse {
+            NotFound("Could not load pinned image")
+          }
         }
 
       } else {
-        logger.info(s"No local file ${filePathForContent(url)}")
+        logger.info(s"No local file ${filePathForContent(url)} for url $url")
         Future.successful(NotFound(Json.toJson("Not found")))
       }
 
@@ -49,7 +53,7 @@ class ThumbnailController @Inject()(val controllerComponents: ControllerComponen
     }
   }
 
-  private def resize(url: URL): Future[Array[Byte]] = {
+  private def resize(url: URL): Future[Option[Array[Byte]]] = {
     // Make a call to image proxy and return the result
     // This ping pong back to ourselves is abit odd but isn't hurting
     val originUrl = cardsUrl + "/pinned?url=" + URLEncoder.encode(url.toExternalForm, "UTF-8")
@@ -64,10 +68,10 @@ class ThumbnailController @Inject()(val controllerComponents: ControllerComponen
     eventualResponse.map { r =>
       r.status match {
         case 200 =>
-          r.bodyAsBytes.toArray
+          Some(r.bodyAsBytes.toArray)
         case _ =>
           logger.error(s"Error fetching thumbnail from image proxy: ${r.status} ${r.body}")
-          Array.empty[Byte]
+          None
       }
     }
   }
